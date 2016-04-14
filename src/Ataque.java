fb59f648736ad5e53f3336cbbdf8587284e481d5
Daniel Hoshi <daniel_hoshi@hotmail.com>
2016-04-14 10:13:15 -0300

    Commit

---
+++ src/Ataque.java
@@ -0,0 +1,18 @@
+
+public class Ataque {
+	private int poder;
+	private String nome;
+	
+	public int getPoder() {
+		return poder;
+	}
+	public void setPoder(int poder) {
+		this.poder = poder;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
